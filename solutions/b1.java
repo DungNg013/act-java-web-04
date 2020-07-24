a2a515fda4b3684991c95cba2e7bd3642a673971
DungNg013 <47709256+DungNg013@users.noreply.github.com>
2020-07-24 21:49:06 +0700

    Dũng Nguyễn - bt mảng hai chiều

--- solutions/b1.java
+++ solutions/b1.java
@@ -3,38 +3,78 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mang_1_chieu;
+package mang_2_chieu;
 
 import java.util.Scanner;
 
 /**
  *
- * @author Dung Ng//
+ * @author Dung Ng
  */
 public class b1 {
     public static void main(String[] args) {
-         int [] arr = new int [5];
-         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();           
+        //khoi tao mang, nhap gia tri
+        int [][] arr = new int[3][3];
+        System.out.println("nhap gia tr mang 3x3");
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = sc.nextInt();
+                
+            }
+            
         }
-        float x = 0, y = 0, z = 0;
-        for (int j = 0; j< arr.length; j++) {
-             x+= arr[j];          
+        //inm nag vua nhap
+        System.out.println("gia tri mang vuan hap");
+         for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr[i][j]);
+                                          
+            }
+             System.out.println("");
+            
         }
-        System.out.println("sum > "+x);
-        for (int m = 0; m < arr.length; m++) {
-            if(arr[m] %2==0){
-                y+= arr[m];
-            } 
+         
+         //tong moi hang
+                 int x=0, y=0, z=0;
+        for (int i = 0; i < 3; i++) {
+             for (int j = 0; j < 3; j++) {
+                if (i==1){
+                    y+=arr[i][j];
+                }
+                else if(i==0){
+                    x+=arr[i][j];
+                }
+                else{
+                    z+=arr[i][j];
+                }
+                
+            }
         }
-        for (int n = 0; n < arr.length; n++) {
-            if(arr[n] %2 !=0){
-                z+= arr[n];
-            }      
+        System.out.println("tong hang 1 la: "+x);
+        System.out.println("tong hang 2 la: "+y);
+        System.out.println("tong hang 3 la: "+z);
+        
+        //tong moi cot
+            int m=0, n=0, q=0;
+        for (int i = 0; i < 3; i++) {
+             for (int j = 0; j < 3; j++) {
+                if (j==1){
+                    m+=arr[i][j];
+                }
+                else if(j==0){
+                    n+=arr[i][j];
+                }
+                else{
+                    q+=arr[i][j];
+                }
+                
+            }
         }
-         System.out.println("even sum > "+y);
-            System.out.println("old sum > "+z);
+        System.out.println("tong cot 1 la: "+n);
+        System.out.println("tong cot 2 la: "+m);
+        System.out.println("tong cot 3 la: "+q);
+        
+        
     }
-    
 }
--- solutions/b2.java
+++ solutions/b2.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mang_1_chieu;
+package mang_2_chieu;
 
 import java.util.Scanner;
 
@@ -13,34 +13,93 @@ import java.util.Scanner;
  */
 public class b2 {
     public static void main(String[] args) {
-         int [] arr = new int [5];
-         int [] brr = new int [5];
-         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();           
-        }
-        int max = arr[0], min = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if(max < arr[i]){
-                max = arr[i];
-            }
+        int [][] arr = new int[3][3];
+          int [][] brr = new int[3][3];
+            int [][] crr = new int[3][3];
             
-        }
-        System.out.println("max > "+max);
-        for (int i = 0; i < arr.length; i++) {
-            if(min > arr[i]){
-                min = arr[i];
-            }
-            
-        }
-        System.out.println("min > "+min);
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i] %3 == 0){
-                 System.out.printf("%d chia het cho 3\n",arr[i]);
+        System.out.println("nhap gia tr mang A");
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = sc.nextInt();
+                
             }
             
         }
         
-    }
+         System.out.println("nhap gia tr mang B");     
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                brr[i][j] = sc.nextInt();
+                
+            }
             
+        }
+                  System.out.println("gia tri mang A vuan hap");
+         for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr[i][j]);
+                                          
+            }
+             System.out.println("");
+            
+        }
+                 System.out.println("gia tri mang B vuan hap");
+         for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(brr[i][j]);
+                                          
+            }
+             System.out.println("");
+            
+        }
+        
+        //tong hai mang
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                crr[i][j] = arr[i][j]+brr[i][j];
+                
+            }
+        }
+                  System.out.println("gia tri mang A vuan hap");
+         for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr[i][j]);
+                                          
+            }
+             System.out.println("");
+            
+        }
+                 System.out.println("gia tri tong hai mang vua nhap");
+         for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(crr[i][j]);
+                System.out.print("");
+                                          
+            }
+             System.out.println("");
+            
+        }
+        
+        //tich hai ma tran
+        for (int i = 0; i < 3; i++) {
+            for (int k = 0;  k < 3; k++) {
+                crr[i][k] = 0;
+                for(int j = 0; j<3 ; j++){
+                 crr[i][k] = crr[i][k] + arr[i][j]*brr[j][k];
+                }
+            }
+        }
+               System.out.println("gia tri tich hai mang vuan hap");
+         for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(crr[i][j]);
+                System.out.print("");
+                        
+                                          
+            }
+             System.out.println("");
+            
+        }
+    }
 }
